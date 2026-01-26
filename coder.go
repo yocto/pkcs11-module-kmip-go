9ee247436cd18a1d44586663b13c2a87cdafea80
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-26 11:55:41 +0100

    Add coder

---
+++ coder.go
@@ -0,0 +1,67 @@
+package main
+
+import "bytes"
+
+// #include "cgo.h"
+import "C"
+import "encoding/binary"
+import "unsafe"
+
+func EncodeByte(_byte C.CK_BYTE) []byte{
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.BigEndian, byte(_byte))
+	return buffer.Bytes()
+}
+
+func EncodeUnsignedLong(ulong C.CK_ULONG) []byte{
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.BigEndian, uint64(ulong))
+	return buffer.Bytes()
+}
+
+func EncodeUnsignedLongAsLength(ulong C.CK_ULONG) []byte{
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.BigEndian, uint32(ulong))
+	return buffer.Bytes()
+}
+
+func EncodeLong(long C.CK_LONG) []byte{
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.BigEndian, int64(long))
+	return buffer.Bytes()
+}
+
+func EncodeMechanism(mechanism C.CK_MECHANISM) []byte {
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.BigEndian, uint64(mechanism.mechanism))
+	binary.Write(buffer, binary.BigEndian, bool(mechanism.pParameter != nil))
+	if bool(mechanism.pParameter != nil) {
+		binary.Write(buffer, binary.BigEndian, uint32(mechanism.ulParameterLen)+4)
+		//TODO: Detect if string. If yes: length prepend (and +4 in above), if not, just object.
+		binary.Write(buffer, binary.BigEndian, uint32(mechanism.ulParameterLen))
+		binary.Write(buffer, binary.BigEndian, unsafe.Slice((*byte)(mechanism.pParameter), mechanism.ulParameterLen))
+	}
+	return buffer.Bytes()
+}
+
+func getByteForTest() C.CK_BYTE{
+    return C.CK_BYTE(0x01);
+}
+
+func getUnsignedLongForTest() C.CK_ULONG{
+    return C.CK_ULONG(0x7E7F8081);
+}
+
+func getLongForTest() C.CK_LONG{
+    return C.CK_LONG(-0x7E7F8081);
+}
+
+func getMechanismForTest() C.CK_MECHANISM {
+	var iv []C.CK_BYTE = []C.CK_BYTE{1, 2, 3, 4, 5, 6, 7, 8}
+
+	return C.CK_MECHANISM{
+		mechanism:      C.CKM_DES_CBC_PAD,
+		pParameter:     C.CK_VOID_PTR(unsafe.SliceData(iv)),
+		ulParameterLen: C.CK_ULONG(len(iv)),
+	}
+}
---
+++ coder_test.go
@@ -0,0 +1,73 @@
+package main
+
+import "bytes"
+import "testing"
+
+func TestEncodeByte(t *testing.T){
+	_byte := getByteForTest()
+
+    encoded := EncodeByte(_byte)
+    expected := [] byte{
+        0x01,
+    }
+
+	if !bytes.Equal(encoded, expected) {
+		t.Errorf("Test for %q failed:\nExpected:\n%v\nGot:\n%v", "EncodeByte", expected, encoded)
+	}
+}
+
+func TestEncodeUnsignedLong(t *testing.T){
+	ulong := getUnsignedLongForTest()
+
+    encoded := EncodeUnsignedLong(ulong)
+    expected := [] byte{
+        0x00,0x00,0x00,0x00,0x7E,0x7F,0x80,0x81,
+    }
+
+	if !bytes.Equal(encoded, expected) {
+		t.Errorf("Test for %q failed:\nExpected:\n%v\nGot:\n%v", "EncodeUnsignedLong", expected, encoded)
+	}
+}
+
+func TestEncodeUnsignedLongAsLength(t *testing.T){
+	ulong := getUnsignedLongForTest()
+
+    encoded := EncodeUnsignedLongAsLength(ulong)
+    expected := [] byte{
+        0x7E,0x7F,0x80,0x81,
+    }
+
+	if !bytes.Equal(encoded, expected) {
+		t.Errorf("Test for %q failed:\nExpected:\n%v\nGot:\n%v", "EncodeUnsignedLongAsLength", expected, encoded)
+	}
+}
+
+func TestEncodeLong(t *testing.T){
+	ulong := getLongForTest()
+
+    encoded := EncodeLong(ulong)
+    expected := [] byte{
+        0xFF,0xFF,0xFF,0xFF,0x81,0x80,0x7F,0x7F,
+    }
+
+	if !bytes.Equal(encoded, expected) {
+		t.Errorf("Test for %q failed:\nExpected:\n%v\nGot:\n%v", "EncodeLong", expected, encoded)
+	}
+}
+
+func TestEncodeMechanism(t *testing.T) {
+	mechanism := getMechanismForTest()
+
+	encoded := EncodeMechanism(mechanism)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x25, // CKM_DES_CBC_PAD
+		0x01,                   // Parameter is present
+		0x00, 0x00, 0x00, 0x0C, // Entire parameter length encoding
+		0x00, 0x00, 0x00, 0x08, // ulParameterLen (for simple CK_BYTE array)
+		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, // pParameter, the IV
+	}
+
+	if !bytes.Equal(encoded, expected) {
+		t.Errorf("Test for %q failed:\nExpected:\n%v\nGot:\n%v", "EncodeMechanism", expected, encoded)
+	}
+}
